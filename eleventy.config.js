6919b6abf4034cc003beded0b8c908f07de2f3c7
347Online | Katie Janzen <katiejanzen@347online.me>
2025-06-19 15:19:30 -0500

    Initial Commit
    Highly-minimal stripped-down starter site

---
+++ eleventy.config.js
@@ -0,0 +1,13 @@
+export default (eleventyConfig) => {
+  eleventyConfig.addPassthroughCopy("src/style.css");
+
+  return {
+    passthroughFileCopy: true,
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+  };
+};
