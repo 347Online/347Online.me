67928d08ef36e79ad2bf9a2f7b475cadc240c948
347Online | Katie Janzen <katiejanzen@347online.me>
2025-06-21 09:31:48 -0500

    Add feeds for feed readers

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,5 @@
 // @ts-check
+
 import { DateTime } from "luxon";
 const TIME_ZONE = "America/Chicago";
 
@@ -32,6 +33,26 @@ function parseDate(dateValue) {
 const postDateFilter = (dateObj) =>
   DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
 
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+const feedConfig = {
+  type: "atom",
+  outputPath: "/blog/feed.xml",
+  collection: {
+    name: "blog",
+    limit: 0, // 0 means no limit
+  },
+  metadata: {
+    language: "en",
+    title: "347Online.me | Blog",
+    subtitle: "",
+    base: "https://347online.me/blog/",
+    author: {
+      name: "Katie Janzen",
+      email: "katiejanzen@347online.me",
+    },
+  },
+};
+
 /** @param {any} eleventyConfig */
 export default (eleventyConfig) => {
   eleventyConfig.addShortcode("excerpt", extraceExcerpt);
@@ -39,6 +60,17 @@ export default (eleventyConfig) => {
   eleventyConfig.addGlobalData("layout", "layout/base.njk");
   eleventyConfig.addDateParsing(parseDate);
   eleventyConfig.addFilter("postDate", postDateFilter);
+  eleventyConfig.addPlugin(feedPlugin, feedConfig);
+  eleventyConfig.addPlugin(feedPlugin, {
+    ...feedConfig,
+    type: "json",
+    outputPath: "/blog/feed.json",
+  });
+  eleventyConfig.addPlugin(feedPlugin, {
+    ...feedConfig,
+    type: "rss",
+    outputPath: "/blog/rss.xml",
+  });
 
   return {
     passthroughFileCopy: true,
