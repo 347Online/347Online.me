ab971f795a0ce4a6fd8e2c1dbef6ae5680f5c43c
347Online | Katie Janzen <katiejanzen@347online.me>
2025-11-05 21:12:27 -0600

    Plugin and config adjustments
    Add plugin for pandoc-style attribute syntax for markdown
    Add plugin for callout cards
    Update eleventy
    Changes to metadata like name, desc
    Increment version

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,7 +5,10 @@ import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 import redirectPlugin from "eleventy-plugin-redirects";
 import embedYouTube from "eleventy-plugin-youtube-embed";
 import { DateTime } from "luxon";
+import MarkdownIt from "markdown-it";
+import markdownItAttrs from "markdown-it-attrs";
 import footnote_plugin from "markdown-it-footnote";
+import MarkdownItGitHubAlerts from "markdown-it-github-alerts";
 
 const TIME_ZONE = "America/Chicago";
 
@@ -67,7 +70,13 @@ export default (eleventyConfig) => {
   eleventyConfig.addGlobalData("layout", "layout/base.njk");
   eleventyConfig.addDateParsing(parseDate);
   eleventyConfig.addFilter("postDate", postDateFilter);
-  eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(footnote_plugin));
+  eleventyConfig.setLibrary(
+    "md",
+    MarkdownIt({ html: true, breaks: true, linkify: true })
+      .use(footnote_plugin)
+      .use(MarkdownItGitHubAlerts)
+      .use(markdownItAttrs),
+  );
   eleventyConfig.addCollection("releasedPosts", (api) =>
     api
       .getFilteredByTag("blog")
