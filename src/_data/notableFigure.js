c171a9ff4dd33f7d92f7aa1a20e39ac159000255
347Online | Katie Janzen <katiejanzen@347online.me>
2026-09-06 09:09:33 -0600

    Tweak language on About Page
    Add "views expressed disclaimer"

---
+++ src/_data/notableFigure.js
@@ -0,0 +1,9 @@
+const figures = [
+  "Phineas Gage",
+  "John Chapman",
+  "Rufus Xavier Sarsaparilla",
+  "Billy Joel Armstrong",
+  "T. Yoshisaur Munchakoopas",
+];
+
+export default figures[Math.round(Math.random() * figures.length)];
