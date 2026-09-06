f7c794326812562089d5c795f55f19bc0b3ae6d8
347Online | Katie Janzen <katiejanzen@347online.me>
2026-09-06 11:13:21 -0600

    Custom plugin for Feed Reader support
    Drop support for JSON feeds

--- eleventy.config.ts
+++ eleventy.config.ts
@@ -1,10 +1,10 @@
 import defineConfig from "11ty.ts";
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
-import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 import redirectPlugin from "eleventy-plugin-redirects";
 import embedYouTube from "eleventy-plugin-youtube-embed";
 import "tsx/esm";
 import { datePlugin } from "./plugins/date-plugin";
+import { feedSubscriptionsPlugin } from "./plugins/feed-subscriptions-plugin";
 import { markdownPlugin } from "./plugins/markdown-plugin";
 import { syntaxPlugin } from "./plugins/syntax-highlight";
 
@@ -16,40 +16,15 @@ const extractExcerpt = ({ templateContent = "" }) => {
   return templateContent;
 };
 
-const atomFeedConfig = {
-  type: "atom",
-  outputPath: "/blog/feed.xml",
-  collection: {
-    name: "releasedPosts",
-    limit: 0, // 0 means no limit
-  },
-  metadata: {
-    language: "en",
-    title: "Katie's Place | 347Online.me",
-    subtitle: "",
-    base: "https://347online.me/blog/",
-    author: {
-      name: "Katie Janzen",
-      email: "katiejanzen@347online.me",
-    },
-  },
-} as const;
-
-const rssFeedConfig = {
-  ...atomFeedConfig,
-  type: "rss",
-  outputPath: "/blog/rss.xml",
-} as const;
-
-const jsonFeedConfig = {
-  ...atomFeedConfig,
-  type: "json",
-  outputPath: "/blog/feed.json",
-} as const;
-
 export default defineConfig((eleventyConfig) => {
+  // Copy these files unmodified directly into the output
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("**/*.pdf");
+
+  // Custom Plugins
   eleventyConfig.addPlugin(datePlugin);
   eleventyConfig.addPlugin(markdownPlugin);
+  eleventyConfig.addPlugin(feedSubscriptionsPlugin);
 
   eleventyConfig.addShortcode("excerpt", extractExcerpt);
   eleventyConfig.addGlobalData("layout", "layout/base.njk");
@@ -60,17 +35,10 @@ export default defineConfig((eleventyConfig) => {
       .filter((x) => new Date().getTime() >= x.date.getTime()),
   );
 
-  eleventyConfig.addPassthroughCopy("src/assets");
-  eleventyConfig.addPassthroughCopy("**/*.pdf");
-
   eleventyConfig.addPlugin(embedYouTube);
   eleventyConfig.addPlugin(syntaxPlugin);
   eleventyConfig.addPlugin(eleventyImageTransformPlugin);
   eleventyConfig.addPlugin(redirectPlugin, { template: "clientSide" });
-
-  eleventyConfig.addPlugin(feedPlugin, atomFeedConfig);
-  eleventyConfig.addPlugin(feedPlugin, jsonFeedConfig);
-  eleventyConfig.addPlugin(feedPlugin, rssFeedConfig);
 
   return {
     passthroughFileCopy: true,
